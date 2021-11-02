d2d8d6b689560a3b70c87f9f629ec23e0a2f0a8a
Leonardo F. Mercado Benitez <lfmercadobe@unal.edu.co>
2021-11-02 01:26:27 -0500

    Se implementa la función start game con el clear board

--- 02_Reto_4_Tic_Tac_Toe/app/src/main/java/com/rey/timefighter/reto0/tictactoe/leonardo/MainActivity.kt
+++ 02_Reto_4_Tic_Tac_Toe/app/src/main/java/com/rey/timefighter/reto0/tictactoe/leonardo/MainActivity.kt
@@ -2,8 +2,27 @@ package com.rey.timefighter.reto0.tictactoe.leonardo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // Declaring Buttons
+    internal lateinit var btn1: Button
+    internal lateinit var btn2: Button
+    internal lateinit var btn3: Button
+    internal lateinit var btn4: Button
+    internal lateinit var btn5: Button
+    internal lateinit var btn6: Button
+    internal lateinit var btn7: Button
+    internal lateinit var btn8: Button
+    internal lateinit var btn9: Button
+    internal lateinit var btnReset: ImageButton
+    internal lateinit var mBoardButtons: List<Button>
+
+    // Declaring Labels
+    internal lateinit var lblInfoGame: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +32,34 @@ class MainActivity : AppCompatActivity() {
         setup()
     }
     fun setup(){
+        // Linking a ids
+        btn1 = findViewById(R.id.btn1)
+        btn2 = findViewById(R.id.btn2)
+        btn3 = findViewById(R.id.btn3)
+        btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
+        mBoardButtons = listOf(btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9)
+        lblInfoGame = findViewById(R.id.lblInfo)
+        btnReset = findViewById(R.id.btnReset)
 
-        val game = TicTacToeGame()
-
+        // Starting a new Game
+        clearBoard()
     }
-    fun clearBoard(){}
+    fun clearBoard(){
+        // clearing the buttons
+        for(btn in mBoardButtons){
+            btn.text = ""
+            btn.isEnabled = true
+            btn.isClickable = true
+        }
+
+        // Clearing the label info
+        lblInfoGame.text = getString(R.string.msgYouTurn)
+    }
     fun setMove(player:Char, location:Int){}
     fun getComputerMove():Int{return 0}
     fun checkForWinner():Int{return 0}

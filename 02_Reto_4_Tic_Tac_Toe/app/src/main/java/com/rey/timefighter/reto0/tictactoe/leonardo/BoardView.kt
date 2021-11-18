6c2e93c6b1ef7ebfdbad3037ebc2272ba5625c5a
Leonardo F. Mercado Benitez <lfmercadobe@unal.edu.co>
2021-11-17 22:21:54 -0500

    Se sobre escribe la funcion onDraw en  boardView

--- 02_Reto_4_Tic_Tac_Toe/app/src/main/java/com/rey/timefighter/reto0/tictactoe/leonardo/BoardView.kt
+++ 02_Reto_4_Tic_Tac_Toe/app/src/main/java/com/rey/timefighter/reto0/tictactoe/leonardo/BoardView.kt
@@ -1,8 +1,7 @@
 package com.rey.timefighter.reto0.tictactoe.leonardo
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -11,19 +10,48 @@ class BoardView(context: Context,
                 attrs: AttributeSet? = null,
                 defStyleAttr: Int=0) : View(context,attrs,defStyleAttr) {
     // Values:
-    public val GRID_WIDTH = 6
+    public val GRID_WIDTH: Float = 6.0f
 
     // Variables:
     private lateinit var mHumanBitmap: Bitmap
     private lateinit var mComputerBitmap: Bitmap
+    private lateinit var mPaint: Paint
 
     init {
         initialize()
+        mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     }
 
     public fun initialize(){
         mHumanBitmap = BitmapFactory.decodeResource(resources,R.drawable.x_img)
         mComputerBitmap = BitmapFactory.decodeResource(resources,R.drawable.o_img)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        // Determine the width and height of the View
+        var boardWidth: Float = width.toFloat()
+        var boardHeight: Float = height.toFloat()
+
+        // Make thick, ligh gray lines
+        mPaint.color = Color.LTGRAY
+        mPaint.strokeWidth = GRID_WIDTH
+
+        // Draw the two vertical board lines
+        var cellWidth: Float = boardHeight / 3;
+        canvas?.drawLine(cellWidth, 0F, cellWidth, boardHeight,mPaint)
+        canvas?.drawLine(cellWidth*2,0F,cellWidth*2,boardHeight,mPaint)
+
+        // Draw the two horizontal lines
+        var cellHeight: Float = boardWidth / 3
+        canvas?.drawLine(cellHeight,0F, cellHeight,boardWidth,mPaint)
+        canvas?.drawLine(cellHeight*2,0F,cellHeight*2,boardWidth,mPaint)
+
+        
+
+
+
     }
 
     
